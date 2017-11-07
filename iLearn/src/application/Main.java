d655acfe75df18712cbab6b9fd37141b200eb85b
trwhit6796 <31318149+trwhit6796@users.noreply.github.com>
2017-11-07 11:37:28 -0500

    Trwhit6796

---
+++ iLearn/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("oreos.fxml"));
+			Scene scene = new Scene(root,600,600);
+			scene.getStylesheets().add(getClass().getResource("oreos.fxml").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ iLearn/src/application/Oreos_view_controller.java
@@ -0,0 +1,5 @@
+package application;
+
+public class Oreos_view_controller {
+
+}
