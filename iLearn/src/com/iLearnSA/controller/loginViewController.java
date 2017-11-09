3b886faad4edf94585409eddb68f65f5c41fcc48
chwong4703 <31318158+chwong4703@users.noreply.github.com>
2017-11-09 11:21:31 -0500

    Testing

--- iLearn/src/com/iLearnSA/controller/loginViewController.java
+++ iLearn/src/com/iLearnSA/controller/loginViewController.java
@@ -1,16 +1,24 @@
 package com.iLearnSA.controller;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import application.iLearnDBConfig;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 
-public class loginViewController {
+public class loginViewController{
 	@FXML private TextField userNameField;
 	@FXML private TextField passwordField;
 	 
@@ -19,36 +27,51 @@ public class loginViewController {
 	
 	public void loginBtnClicked() throws SQLException {
 		connection = iLearnDBConfig.getConnection();
-		String userName = userNameField.getText();
-//		if (!(userName.length() > 0))	
-//			System.out.println("userName Field Required");
-//		else
+		String usernameInput = userNameField.getText();
+		String passwordInput = passwordField.getText();
+		
+		if((usernameInput == null || usernameInput.isEmpty()) ||
+			(passwordInput == null || passwordInput.isEmpty())) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Input fields empty");
+			alert.setContentText("Please fill usname and password fields");
+			alert.showAndWait();
+		}
+		else {
 			try {
 				// try type username with "chwong4703" in userName textField
-				//String sql_query = "SELECT * FROM USER WHERE Username = '" + userName + "'"; 
-				String sql_query = "SELECT * FROM USER"; 
+				String sql_query = "SELECT * FROM USER WHERE Username = '" + usernameInput + "'"; 
 				// create the java statement
 				Statement st = connection.createStatement();
 			    // execute the query, and get a java resultset
 			    ResultSet rs = st.executeQuery(sql_query);
 			    // iterate through the java resultset
-			      while (rs.next())
-			      {
-			    	int id = rs.getInt("id");
-			        String firstName = rs.getString("FirstName");
-			        String lastName = rs.getString("LastName");
-			        String email = rs.getString("Email");
-			        String username = rs.getString("Username");
-			        String password = rs.getString("Password");
-			        System.out.println("First Name: " + firstName + "\tLastName: " + lastName + 
-			        					"\tEmail: " + email + "\tUsername: " + username + "\tpassword: " + password);
-			      }
-			      st.close();
+			    rs.first();
+			    String password = rs.getString("Password");
+			    if(passwordInput.equals(password)) {
+			    	
 			    }
-			    catch (Exception e)
-			    {
+			    st.close();
+		    }catch(Exception e){
 			      System.err.println("Got an exception! ");
 			      System.err.println(e.getMessage());
-			    }
+			}
+		}
+	}
+
+	public void signUpBtnClicked(ActionEvent event) throws IOException {
+		Parent view = FXMLLoader.load(getClass().getResource("../view/signUpView.fxml"));
+		Scene scene = new Scene(view);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+	
+	public void loginBtnClicked(ActionEvent event) throws IOException {
+		Parent view = FXMLLoader.load(getClass().getResource(""));
+		Scene scene = new Scene(view);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
 	}
 }
